f37b0e54c9e65c380b04fe8ff1b1b7a9e511bfdb
sushanth <sushanth.nukala14@gmail.com>
2021-06-21 13:46:34 +0530

    AddBook Component Setup

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import ApolloClient from "apollo-boost";
 import { ApolloProvider } from 'react-apollo';
 
 import BookList from "./components/BookList";
+import AddBook from "./components/AddBook";
 
 const client = new ApolloClient({
   uri: 'http://localhost:4000/graphql'
@@ -13,6 +14,7 @@ function App() {
       <div id="main">
         <h1>Learning GraphQL</h1>
         <BookList />
+        <AddBook />
       </div>
     </ApolloProvider>
   );
