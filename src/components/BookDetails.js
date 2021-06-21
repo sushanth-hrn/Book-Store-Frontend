e3da77aca0a17a3620027bec890f0592ee31a017
sushanth <sushanth.nukala14@gmail.com>
2021-06-22 00:09:57 +0530

    updating the UI when to show the bookdetails when user clicks on a book

--- src/components/BookDetails.js
+++ src/components/BookDetails.js
@@ -3,19 +3,47 @@ import { graphql } from 'react-apollo';
 import { getBookQuery } from "../queries/queries";
 
 class BookDetail extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-
+    displayBookDetails(){
+        const { book } = this.props.data;
+        if(book){
+            return(
+                <div>
+                    <h1>{book.name}</h1>
+                    <p>{book.genre}</p>
+                    <p>{book.author.name}</p>
+                    <p>All books by this author</p>
+                    <ul className="other-books">
+                        {
+                            book.author.books.map((item) => {
+                                return <li key={item.id}>{item.name}</li>
+                            })
+                        }
+                    </ul>
+                </div>
+            );
+        }else{
+            return (
+                <div>
+                    No book selected . . .
+                </div>
+            )
         }
     }
     render() { 
         return (
             <div id="book-details">
-                <p>Book Details will be displayed here</p>
+                {this.displayBookDetails()}
             </div>
         );
     }
 }
  
-export default graphql(getBookQuery)(BookDetail);
+export default graphql(getBookQuery,{
+    options: (props) => {
+        return {
+            variables: {
+                id: props.bookId
+            }
+        }
+    }
+})(BookDetail);
--- src/components/BookList.js
+++ src/components/BookList.js
@@ -4,6 +4,14 @@ import { getBooksQuery } from "../queries/queries";
 import BookDetails from './BookDetails';
 
 class BookList extends Component {
+
+    constructor(props){
+        super(props);
+        this.state = {
+            selected: null
+        }
+    }
+
     displayBooks(){
         const data = this.props.data;
         if(data.loading){
@@ -12,7 +20,7 @@ class BookList extends Component {
             );
         } else{
             return data.books.map((book) => {
-                return <li key={book.id}>{book.name}</li>
+                return <li key={book.id} onClick={(e) => this.setState({ selected : book.id })}>{book.name}</li>
             })
         }
     }
@@ -23,7 +31,7 @@ class BookList extends Component {
                 <ul id="book-list">
                     {this.displayBooks()}
                 </ul>
-                <BookDetails />
+                <BookDetails bookId={this.state.selected}/>
             </div>
         );
     }
